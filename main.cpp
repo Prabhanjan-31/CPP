a8fe1e508b048af12a2f10015da39546822089aa
Prabhanjan <Prabhanjan3031@gmail.com>
2026-05-12 22:43:23 +0530

    testing webhook

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Header file for input/output
 
 int main() { // Entry point of the program
-    std::cout << "Hello, World!"; // Output statement
+    std::cout << "Hello, World!"<<std::endl; // Output statement
     return 0; // Standard exit status
 }
